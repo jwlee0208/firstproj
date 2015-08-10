ebce79a1bfa6c353ec83e787cd1b25c278530934
이진원 <jwlee0208@wemakeprice.com>
2015-08-10 11:26:41 +0900

    #1 이슈 처리 - 특정 컬럼 데이터의 암호화 1. 사용자 정보 테이블의 이메일, 전화번호가 대상 2. aes256

--- workspace/firstproj/src/main/java/com/firstproj/user/service/UserServiceImpl.java
+++ workspace/firstproj/src/main/java/com/firstproj/user/service/UserServiceImpl.java
@@ -2,13 +2,20 @@ package com.firstproj.user.service;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
+import com.firstproj.common.CommonConstant;
+import com.firstproj.common.util.AES256Util;
 import com.firstproj.user.dao.UserDao;
 import com.firstproj.user.dto.UserDto;
 
 @Service("UserServiceImpl")
 public class UserServiceImpl implements UserService{
+    
+    final Log logger = LogFactory.getLog(this.getClass());
+    
 	@Resource(name="UserDao")
 	private UserDao userDao; 
 
@@ -19,6 +26,22 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public int insertUserInfo(UserDto userDto) throws Exception{
+	    
+	    if(userDto != null){
+	        String     email       = userDto.getEmail();
+	        String     phoneNo     = userDto.getPhoneNo();
+	        AES256Util aes256util  = new AES256Util(CommonConstant.IV);
+	        
+	        if(!email.isEmpty()){
+	            String     encEmail    = aes256util.encrypt(email);    
+	            userDto.setEmail(encEmail);
+	        }
+	        if(!phoneNo.isEmpty()){
+	            String     encPhoneNo  = aes256util.encrypt(phoneNo);
+	            userDto.setPhoneNo(encPhoneNo);	            
+	        }
+	    }
+	    
 		return this.userDao.insertUserInfo(userDto);
 	}
 	
